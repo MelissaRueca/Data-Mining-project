19adfaa297687982e257074073c627f9068167cd
evitadologlou <evitadologlou@gmail.com>
2025-10-15 19:38:23 +0200

    newww

--- Multinomial_Naive_Bayes.py
+++ Multinomial_Naive_Bayes.py
@@ -1,122 +1,109 @@
-import pandas as pd
 import numpy as np
-import pickle
+import pandas as pd
 from sklearn.feature_selection import SelectKBest, chi2
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report
+from sklearn.feature_extraction.text import TfidfVectorizer
+from pr import prepare_data  
 
-# Load processed data
-train_df = pd.read_csv("train_reviews.csv")
-test_df  = pd.read_csv("test_reviews.csv")
-y_train = train_df["label"]
-y_test  = test_df["label"]
-
-# Load vectorizers
-with open("tfidf_uni.pkl", "rb") as f:
-    tfidf_uni = pickle.load(f)
-
-with open("tfidf_bi.pkl", "rb") as f:
-    tfidf_bi = pickle.load(f)
-
-# Transform data
-X_train_uni = tfidf_uni.transform(train_df["clean_text"])
-X_test_uni  = tfidf_uni.transform(test_df["clean_text"])
-
-X_train_bi = tfidf_bi.transform(train_df["clean_text"])
-X_test_bi  = tfidf_bi.transform(test_df["clean_text"])
+#  Load and preprocess data
+train_df, test_df, y_train, y_test = prepare_data()
 
 
-# UNIGRAMS 
-print(" Multinomial Naive Bayes - Unigrams")
+#  Multinomial Naive Bayes - Unigrams
+
+print("Multinomial Naive Bayes - Unigrams")
 
 pipe_uni = Pipeline([
+    ('tfidf', TfidfVectorizer(max_df=0.95, min_df=2,ngram_range=(1,1))),
     ('select', SelectKBest(score_func=chi2)),
     ('nb', MultinomialNB())
 ])
 
+
 param_grid_uni = {
-    'select__k': [500, 1000, 1500, 2000, 2500],
-    'nb__alpha': [0.01, 0.1, 0.5, 1.0, 5.0]
+    'tfidf__max_features': [2000, 5000, 8000,10000],
+    'select__k': [500, 1000, 1500, 2000], 
+    'nb__alpha': [0.01, 0.1, 0.5, 1.0]
 }
 
 grid_uni = GridSearchCV(pipe_uni, param_grid_uni, cv=10, n_jobs=-1)
-grid_uni.fit(X_train_uni, y_train)
+grid_uni.fit(train_df["clean_text"], y_train)
 
 print("Best parameters:", grid_uni.best_params_)
 
-y_pred_uni = grid_uni.predict(X_test_uni)
+y_pred_uni = grid_uni.predict(test_df["clean_text"])
 
 print("Accuracy:", accuracy_score(y_test, y_pred_uni))
 print("Precision:", precision_score(y_test, y_pred_uni, pos_label='deceptive'))
 print("Recall:", recall_score(y_test, y_pred_uni, pos_label='deceptive'))
 print("F1-score:", f1_score(y_test, y_pred_uni,pos_label='deceptive'))
-print("Classification Report:", classification_report(y_test, y_pred_uni))
+print("Classification Report:\n", classification_report(y_test, y_pred_uni))
 
 # Top 5 features - Unigrams
 
 # Get trained components
-nb_model = grid_uni.best_estimator_.named_steps['nb']
-selector = grid_uni.best_estimator_.named_steps['select']
+tfidf_uni_best = grid_uni.best_estimator_.named_steps['tfidf']
+selector_uni = grid_uni.best_estimator_.named_steps['select']
+nb_model_uni = grid_uni.best_estimator_.named_steps['nb']
 
-# Feature names after selection
-feature_names = np.array(tfidf_uni.get_feature_names_out())[selector.get_support()]
+# Feature names after selection 
+feature_names_uni = np.array(tfidf_uni_best.get_feature_names_out())[selector_uni.get_support()]
 
-# Log probabilities of features for each class
-feature_log_probs = nb_model.feature_log_prob_  # shape (n_classes, n_features)
-
-# Difference in log probabilities between classes
-log_prob_diff = feature_log_probs[1] - feature_log_probs[0]
+# Log probabilities of features for each class and Difference in log probabilities between classes
+log_prob_diff_uni = nb_model_uni.feature_log_prob_[1] - nb_model_uni.feature_log_prob_[0]
 
 # Top 5 features for deceptive (class 1) and truthful (class 0) reviews
-top_k_deceptive = np.argsort(log_prob_diff)[-5:][::-1]
-top_k_truthful = np.argsort(-log_prob_diff)[-5:][::-1]
+top_k_deceptive_uni = np.argsort(log_prob_diff_uni)[-5:][::-1]
+top_k_truthful_uni = np.argsort(-log_prob_diff_uni)[-5:][::-1]
 
-print("Top 5 features for deceptive reviews:", feature_names[top_k_deceptive])
-print("Top 5 features for truthful reviews:", feature_names[top_k_truthful])
+print("Top 5 features for deceptive reviews:", feature_names_uni[top_k_deceptive_uni])
+print("Top 5 features for truthful reviews:", feature_names_uni[top_k_truthful_uni])
 
+#  Multinomial Naive Bayes - Unigrams + Bigrams
 
-#  UNIGRAMS AND BIGRAMS 
-print("Multinomial Naive Bayes - Unigrams and Bigrams ")
+print("Multinomial Naive Bayes - Unigrams + Bigrams")
 
 pipe_bi = Pipeline([
+    ('tfidf', TfidfVectorizer(max_df=0.95,min_df=2,ngram_range=(1,2))),
     ('select', SelectKBest(score_func=chi2)),
     ('nb', MultinomialNB())
 ])
 
+
 param_grid_bi = {
-    'select__k': [1000, 2000, 3000, 5000, 8000],
-    'nb__alpha': [0.01, 0.1, 0.5, 1.0, 5.0]
+    'tfidf__max_features': [5000, 8000, 10000,120000],
+    'select__k': [1000, 2000, 3000, 5000],  
+    'nb__alpha': [0.01, 0.1, 0.5, 1.0]
 }
 
 grid_bi = GridSearchCV(pipe_bi, param_grid_bi, cv=10, n_jobs=-1)
-grid_bi.fit(X_train_bi, y_train)
+grid_bi.fit(train_df["clean_text"], y_train)
 
 print("Best parameters:", grid_bi.best_params_)
 
-y_pred_bi = grid_bi.predict(X_test_bi)
+y_pred_bi = grid_bi.predict(test_df["clean_text"])
 
 print("Accuracy:", accuracy_score(y_test, y_pred_bi))
 print("Precision:", precision_score(y_test, y_pred_bi, pos_label='deceptive'))
 print("Recall:", recall_score(y_test, y_pred_bi,pos_label='deceptive'))
 print("F1-score:", f1_score(y_test, y_pred_bi, pos_label='deceptive'))
-print("Classification Report:", classification_report(y_test, y_pred_bi))
+print("Classification Report:\n", classification_report(y_test, y_pred_bi))
 
 # Top 5 features - Unigrams + Bigrams
 
 # Get trained components
-nb_model_bi = grid_bi.best_estimator_.named_steps['nb']
+tfidf_bi_best = grid_bi.best_estimator_.named_steps['tfidf']
 selector_bi = grid_bi.best_estimator_.named_steps['select']
+nb_model_bi = grid_bi.best_estimator_.named_steps['nb']
 
-# Feature names after selection
-feature_names_bi = np.array(tfidf_bi.get_feature_names_out())[selector_bi.get_support()]
+# Feature names after selection 
+feature_names_bi = np.array(tfidf_bi_best.get_feature_names_out())[selector_bi.get_support()]
 
-# Log probabilities of features for each class
-feature_log_probs_bi = nb_model_bi.feature_log_prob_  
-
-# Difference in log probabilities between classes
-log_prob_diff_bi = feature_log_probs_bi[1] - feature_log_probs_bi[0]
+# Log probabilities of features for each class and Difference in log probabilities between classes
+log_prob_diff_bi = nb_model_bi.feature_log_prob_[1] - nb_model_bi.feature_log_prob_[0]
 
 # Top 5 features for deceptive (class 1) and truthful (class 0) reviews
 top_k_deceptive_bi = np.argsort(log_prob_diff_bi)[-5:][::-1]
@@ -125,16 +112,24 @@ top_k_truthful_bi = np.argsort(-log_prob_diff_bi)[-5:][::-1]
 print("Top 5 features for deceptive reviews:", feature_names_bi[top_k_deceptive_bi])
 print("Top 5 features for truthful reviews:", feature_names_bi[top_k_truthful_bi])
 
-#Save preditcions for unigrams 
+
+#  Save predictions
+
 pd.DataFrame({
     "text": test_df["clean_text"],
     "true_label": y_test,
     "predicted_label": y_pred_uni
 }).to_csv("predictions_MNB_unigrams.csv", index=False)
 
-#Save predictions for unigrams + bigrams 
 pd.DataFrame({
     "text": test_df["clean_text"],
     "true_label": y_test,
     "predicted_label": y_pred_bi
 }).to_csv("predictions_MNB_unigramsandbigrams.csv", index=False)
+
+
+
+
+
+
+
